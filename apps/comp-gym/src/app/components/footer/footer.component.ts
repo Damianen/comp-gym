3f019942de4f24e8cedfa7d57cd5fa04c152c4cf
Damianen <113175544+Damianen@users.noreply.github.com>
2024-11-19 12:42:34 +0100

    Add homepage with footer, header and hero section

--- apps/comp-gym/src/app/components/footer/footer.component.ts
+++ apps/comp-gym/src/app/components/footer/footer.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-footer',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './home.component.html',
+  templateUrl: './footer.component.html',
 })
-export class HomeComponent {}
+export class FooterComponent {}
---
+++ apps/comp-gym/src/app/components/root/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+import { HomeComponent } from '../home/home.component';
+
+
+@Component({
+  standalone: true,
+  imports: [RouterModule, HeaderComponent, FooterComponent, HomeComponent],
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'comp-gym';
+}
