ad4585c81666916cda81f20cd7ca28bda3447c57
Damianen <113175544+Damianen@users.noreply.github.com>
2024-12-15 20:46:13 +0100

    quick fix

--- libs/shared/util-env/src/lib/environment.development.ts
+++ libs/shared/util-env/src/lib/environment.development.ts
@@ -4,9 +4,9 @@ export const environment: IEnvironment = {
 	production: false,
 
 	ROOT_DOMAIN_URL: 'http://localhost:3000',
-	API_URL: 'http://localhost:3000/api/',
+	API_URL: 'https://comp-gym-api-h3azbjbcemhnftcb.westeurope-01.azurewebsites.net/',
 
-	RCMD_API_URL: 'http://localhost:3100/api/',
+	RCMD_API_URL: 'https://comp-gym-api-h3azbjbcemhnftcb.westeurope-01.azurewebsites.net/',
 
 	MONGO_DB_CONNECTION_STRING: 'mongodb://localhost:27017/shareameal',
 };
