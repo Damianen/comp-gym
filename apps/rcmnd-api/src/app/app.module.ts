7905fc70fa36693c0fc3b580facb7d732f1f46b8
Damian Buskens <113175544+Damianen@users.noreply.github.com>
2024-12-16 09:17:52 +0100

    Update app.module.ts

--- apps/rcmnd-api/src/app/app.module.ts
+++ apps/rcmnd-api/src/app/app.module.ts
@@ -3,16 +3,7 @@ import { Module } from '@nestjs/common';
 import { Neo4jModule } from 'nest-neo4j';
 
 @Module({
-	imports: [
-		Neo4jModule.forRoot({
-			host: process.env.NEO4J_URI,
-			port: 7687,
-			username: process.env.NEO4J_USERNAME,
-			password: process.env.NEO4J_PASSWORD,
-			scheme: 'bolt+s',
-		}),
-		Neo4jBackendModule,
-	],
+	imports: [],
 	controllers: [],
 	providers: [],
 })
