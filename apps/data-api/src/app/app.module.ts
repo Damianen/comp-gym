1dc5ee9d061fa12233139742647916f33178b7e8
Damian Buskens <113175544+Damianen@users.noreply.github.com>
2024-12-16 09:19:36 +0100

    Update app.module.ts

--- apps/data-api/src/app/app.module.ts
+++ apps/data-api/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { UserModule } from '@comp-gym/backend/user';
 import { MongooseModule } from '@nestjs/mongoose';
 import { WorkoutModule, ExerciseModule } from '@comp-gym/backend/features';
 import { AuthModule } from '@comp-gym/auth';
+import { Neo4jModule } from 'nest-neo4j';
+import { Neo4jBackendModule } from '@comp-gym/neo4j';
 
 @Module({
 	imports: [
@@ -19,6 +21,14 @@ import { AuthModule } from '@comp-gym/auth';
 				return connection;
 			},
 		}),
+		Neo4jModule.forRoot({
+			host: process.env.NEO4J_URI,
+			port: 7687,
+			username: process.env.NEO4J_USERNAME,
+			password: process.env.NEO4J_PASSWORD,
+			scheme: 'bolt+s',
+		}),
+		Neo4jBackendModule,
 	],
 	controllers: [],
 	providers: [],
